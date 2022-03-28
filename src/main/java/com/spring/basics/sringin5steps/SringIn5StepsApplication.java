a897f72723ff2335ba63b2ec3c252b6401c8ccfa
nechaudhary <nechaudhary@paypal.com>
2022-03-28 16:48:15 +0530

    initial commit

---
+++ src/main/java/com/spring/basics/sringin5steps/SringIn5StepsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.basics.sringin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringIn5StepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/basics/sringin5steps/SringIn5StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.basics.sringin5steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringIn5StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
