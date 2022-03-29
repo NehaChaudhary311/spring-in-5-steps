5cbf418318be807e126bb9cdba0f6894fec6551b
nechaudhary <nechaudhary@paypal.com>
2022-03-29 21:35:30 +0530

    removed Autowired error

--- src/main/java/com/spring/basics/sringin5steps/BinarySearchImp.java
+++ src/main/java/com/spring/basics/sringin5steps/BinarySearchImp.java
@@ -1,7 +1,9 @@
 package com.spring.basics.sringin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BinarySearchImp {
     @Autowired
     private SortAlgorithm sortAlgorithm;
--- src/main/java/com/spring/basics/sringin5steps/SringIn5StepsApplication.java
+++ src/main/java/com/spring/basics/sringin5steps/SringIn5StepsApplication.java
@@ -8,9 +8,7 @@ import org.springframework.context.ApplicationContext;
 public class SringIn5StepsApplication {
 
 	public static void main(String[] args) {
-
-
-		ApplicationContext applicationContext = SpringApplication.run(SringIn5StepsApplication.class, args);
+	ApplicationContext applicationContext = SpringApplication.run(SringIn5StepsApplication.class, args);
 		BinarySearchImp binarySearch = applicationContext.getBean(BinarySearchImp.class);
 		int res = binarySearch.binarySearch(new int[] {2,5,6,3}, 3);
 		System.out.println(res);
